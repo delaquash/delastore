0f9e6182adf4ca20110d2ef70ac957d41b726f3f
delaquash <olaide1191@gmail.com>
2023-09-12 11:47:43 +0100

    initial commit

---
+++ backend/server.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import connectDB from "./config/db";
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+
+connectDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API IS RUNNING...");
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on mode on port ${PORT}`);
+  });
---
+++ mobile/navigation/StackNavigator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import LoginScreen from '../screen/LoginScreen';
+import RegisterScreen from '../screen/RegisterScreen';
+
+export type ScreenStackParamList = {
+    Login: undefined
+    Register: undefined
+
+}
+const Stack = createNativeStackNavigator<ScreenStackParamList>()
+
+const StackNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+            component={LoginScreen} 
+            name='Login'
+            options={{ headerShown : false }}
+        />
+        <Stack.Screen 
+            component={RegisterScreen} 
+            name='Register'
+            options={{ headerShown : false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default StackNavigator
