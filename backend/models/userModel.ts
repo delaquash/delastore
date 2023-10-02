16e77a13862ff3e9c3b20cda05ede52030b549e1
delaquash <olaide1191@gmail.com>
2023-10-02 22:44:37 +0100

    fixed: userModel

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,49 +1,49 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import { IUserProps } from "../types/userTypes";
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verifiedToken: {
+    type: String,
+  },
+  addresses: [
+    {
+      name: String,
+      mobile: Number,
+      houseNo: String,
+      street: String,
+      landmark: String,
+      city: String,
+      country: String,
+      postalCode: String,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true
+  ],
+  order: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
     },
-    password:{
-        type: String,
-        required: true
-    },
-    verified:{
-        type: Boolean,
-        default: false
-    },
-    verifiedToken:{
-        type: String
-    },
-    addresses:[
-        {
-            name: String,
-            mobile: Number,
-            houseNo: String,
-            street: String,
-            landmark: String,
-            city: String,
-            country: String,
-            postalCode: String
-        }
-    ],
-    order:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Order"
-    }
-],
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
-})
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 export const User = mongoose.model<IUserProps>("User", userSchema);
