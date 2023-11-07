8596e0f3baae86ac9e3a525b4ff4c86fc75b40ca
delaquash <olaide1191@gmail.com>
2023-11-07 13:12:59 +0100

    feat: cart screen

--- mobile/screen/Cart.tsx
+++ mobile/screen/Cart.tsx
@@ -1,5 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  TextInput,
+  Image,
+} from "react-native";
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import { incrementQuantity, reduceQuantity, removeFromCart} from "../reducers/cartReducer";
 
 const Cart = () => {
   return (
