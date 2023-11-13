67ee9cdbef029682393a81e9539ae4df8fa526b0
delaquash <olaide1191@gmail.com>
2023-11-13 18:53:52 +0100

    feat: add confirm screen details

--- mobile/screen/ConfirmationScreen.tsx
+++ mobile/screen/ConfirmationScreen.tsx
@@ -1,8 +1,9 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { userType } from '../context/useContext';
-
+import { AntDesign, Entypo, Feather, MaterialIcons } from "@expo/vector-icons";
+import { IAddressProps } from '../types/dataType';
 interface StepsData {
   title: string;
   content: string
@@ -16,7 +17,7 @@ const ConfirmationScreen = () => {
     {title: "Place Order", content: "Order Summary"}
   ]
   const [currentStep, setCurrentStep] = useState<number>(0)
-  const [addresses, setAddresses] = useState([])
+  const [addresses, setAddresses] = useState<IAddressProps[]>([])
   const {setUserId, userId} = useContext(userType)
 
   useEffect(() => {
@@ -61,7 +62,73 @@ const ConfirmationScreen = () => {
         </View>
             {currentStep === 0 && (
               <View style={{ marginHorizontal: 20 }}>
-                <Text>Select Delivery Address</Text>
+                <Text style={{ fontSize: 16, fontWeight: "bold"}}>Select Delivery Address</Text>
+                <Pressable>
+                  {addresses.map((address, index)=> (
+                    <Pressable>
+                      <Entypo 
+                        name='circle'
+                        size={24}
+                        color="black"
+                      />
+                      <View>
+                      <View
+                                style={{ flexDirection: "row", alignItems: "center", gap: 3 }}
+                            >
+                                <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+                                    {address?.name}
+                                </Text>
+                                <Entypo name="location-pin" size={24} color="red" />
+                            </View>
+
+                            <Text style={styles.address}>
+                                {address?.houseNo}, {address?.landmark}
+                            </Text>
+
+                            <Text style={styles.address}>
+                                {address?.street}
+                            </Text>
+
+                            <Text style={styles.address}>
+                                Lagos, Nigeria
+                            </Text>
+
+                            <Text style={styles.address}>
+                                phone No : {address?.mobileNo}
+                            </Text>
+                            <Text style={styles.address}>
+                                pin code : {address?.postalCode}
+                            </Text>
+
+                            <View
+                                style={{
+                                    flexDirection: "row",
+                                    alignItems: "center",
+                                    gap: 10,
+                                    marginTop: 7,
+                                }}
+                            >
+                                <Pressable
+                                    style={styles.edit}
+                                >
+                                    <Text>Edit</Text>
+                                </Pressable>
+
+                                <Pressable
+                                    style={styles.remove}
+                                >
+                                    <Text>Remove</Text>
+                                </Pressable>
+
+                                <Pressable style={styles.default}>
+                                
+                                    <Text>Set as Default</Text>
+                                </Pressable>
+                            </View>
+                      </View>
+                    </Pressable>
+                  ))}
+                </Pressable>
               </View>
             )}
       </View>
@@ -90,5 +157,33 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 16,
     fontWeight: "bold"
-  }
+  },
+  address: { 
+    fontSize: 15, 
+    color: "#181818" 
+  },
+  edit:{
+    backgroundColor: "#F5F5F5",
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    borderRadius: 5,
+    borderWidth: 0.9,
+    borderColor: "#D0D0D0",
+},
+  remove: {
+    backgroundColor: "#F5F5F5",
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    borderRadius: 5,
+    borderWidth: 0.9,
+    borderColor: "#D0D0D0",
+},
+  default: {
+    backgroundColor: "#F5F5F5",
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    borderRadius: 5,
+    borderWidth: 0.9,
+    borderColor: "#D0D0D0",
+}
 })
