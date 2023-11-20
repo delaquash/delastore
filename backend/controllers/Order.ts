4536701af1278c8f7c1e5f2aa0c0f1149ebe456a
delaquash <olaide1191@gmail.com>
2023-11-20 08:52:41 +0100

    fix: create product aray from cartItems

---
+++ backend/controllers/Order.ts
@@ -0,0 +1,44 @@
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import { Order } from "../models/OrderModel";
+import { NextFunction, Request, Response } from "../types/express";
+import errorHandler from "../utils/errorMessage";
+import { generateSecretkey, sendVerificationEmail } from "../utils/verifyEmail";
+import { User } from "../models/userModel";
+
+interface cartProps {
+  name: string;
+  price: number;
+  image: string;
+  quantity: number;
+}
+
+/**
+ * Create new order
+ * @route POST /orders
+ * @access Private
+ */
+
+const orders = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { userId, cartItems, totalPrice, shippingAddress, paymentMethod } =
+      req.body;
+    const user = User.findById(userId);
+    if (!user) {
+      const message = "User not found. Please sign up...";
+      const error = errorHandler(message, 422);
+      return next(error);
+    }
+    //   create an array of products objects form cartItems
+    const product = cartItems.map((item: cartProps) => ({
+      name: item?.name,
+      image: item?.image,
+      price: item?.price,
+      quantity: item?.quantity,
+    }));
+  } catch (error) {
+    if (!error.status) error.status = 500;
+    next(error);
+    console.log(error);
+  }
+};
